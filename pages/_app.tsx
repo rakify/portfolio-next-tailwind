af3d0b384bfd1807ec1f82b20fdc63639a161c14
rakiblh <rakib@lemonhivee.com>
2023-06-18 11:11:52 +0600

    refactor: update styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
-import "@/styles/globals.css";
-import { ThemeProvider } from "next-themes";
-import type { AppProps } from "next/app";
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import '@/styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
-        <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-gray-100 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark items-center">
+      <div className="my-14 grid grid-cols-12 gap-6 px-5 sm:px-20 md:mb-16 md:px-32 lg:mb-0 lg:px-36 xl:px-48 ">
+        <div className="col-span-12 h-full items-center rounded-2xl bg-white p-4 text-center text-base shadow-custom-light dark:bg-gray-100 dark:shadow-custom-dark lg:col-span-3">
           <Sidebar />
         </div>
-        <div className="flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark rounded-2xl lg:col-span-9 dark:bg-gray-100">
+        <div className="col-span-12 flex flex-col overflow-hidden rounded-2xl bg-white shadow-custom-light dark:bg-gray-100 dark:shadow-custom-dark lg:col-span-9">
           <Navbar />
           <Component {...pageProps} />
         </div>
