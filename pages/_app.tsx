e3ad1681135b519cc9e7f6cdaeeee032e5fa0f5f
rakify <irakibm@gmail.com>
2023-06-26 00:44:19 +0600

    fix style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,11 @@ import type { AppProps } from "next/app"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <div className="my-14 grid grid-cols-12 gap-6 px-5 sm:px-20 md:mb-16 md:px-32 lg:mb-0 lg:px-36 xl:px-48 ">
-        <div className="col-span-12 h-full items-center rounded-2xl bg-white p-4 text-center text-base shadow-custom-light dark:bg-gray-100 dark:shadow-custom-dark lg:col-span-3">
+      <div className="my-14 grid grid-cols-12 gap-6 px-2 sm:px-40 md:mb-8 md:px-40 lg:mb-0 lg:px-14 xl:px-20 ">
+        <div className="col-span-12 h-full items-center rounded-2xl bg-white p-4 text-center text-base shadow-custom-light dark:bg-gray-100 dark:shadow-custom-dark lg:col-span-4">
           <Sidebar />
         </div>
-        <div className="col-span-12 flex flex-col overflow-hidden rounded-2xl bg-white shadow-custom-light dark:bg-gray-100 dark:shadow-custom-dark lg:col-span-9">
+        <div className="col-span-12 flex flex-col overflow-hidden rounded-2xl bg-white shadow-custom-light dark:bg-gray-100 dark:shadow-custom-dark lg:col-span-8">
           <Navbar />
           <Component {...pageProps} />
         </div>
